1a93ba40caaf3d185b660d33706f8816d686076d
andreas <s160419047@student.ubaya.ac.id>
2023-05-08 10:01:43 +0700

    update scan qr and admin template

--- resources/views/kasir/scan.blade.php
+++ resources/views/kasir/scan.blade.php
@@ -8,49 +8,21 @@
         </div><br>
         <div class="card">
             <div class="card-body">
-                <p class="card-text">Total Harga &nbsp;&nbsp;&nbsp;&nbsp; Rp. 15.000</p>
+               <h4 class="card-title heading-6">Petunjuk Penggunaan</h4><br>
+                <p class="card-text"><b>1. Arahkan QR Code ke kotak kamera</b></p><br>
+                <p class="card-text"><b>2. Tunggu hingga muncul halaman detail pembayaran</b></p><br>
                 <input type="hidden" name="result" id="result">
             </div>
         </div><br>
     </div>
 @endsection
 @section('javascript')
-    <script src="https://unpkg.com/html5-qrcode" type="text/javascript">
+    <script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
     <script>
         function onScanSuccess(decodedText, decodedResult) {
             // handle the scanned code as you like, for example:
             console.log(`Code matched = ${decodedText}`, decodedResult);
             alert(decodedText);
-            // let id = decodedText;
-            // html5QrcodeScanner.clear().then{_=> {
-            //    var CSRF_TOKEN = $('meta[name="csrf_token"]').attr('content');
-            //    $.ajax({
-            //       url: "{{ route('validasi') }}",
-            //       type: 'POST',
-            //       data: {
-            //          _method: 'POST',
-            //          _token: CSRF_TOKEN,
-            //          qr_code: id 
-            //       },
-            //       success: function(response) {
-            //          if(response.status = 404) {
-            //             Swal.fire({
-            //                title: 'QR Code Tidak terdeteksi, Ingin Scan Ulang ?',
-            //                confirmButtonText: 'Ya, scan ulang',
-            //                denyButtonText: 'CANCEL',
-            //             }).then(result) => {
-            //                if(result.isConfirmed) {
-            //                   location.reload();
-            //                } else if(result.isDismissed) {
-            //                   console.log("deny");
-            //                }
-            //             }
-            //          } else {
-                        
-            //          }
-            //       }
-            //    })
-            // }}
         }
 
         function onScanFailure(error) {
@@ -66,4 +38,5 @@
          );
          html5QrcodeScanner.render(onScanSuccess, onScanFailure);
     </script>
+
 @endsection
