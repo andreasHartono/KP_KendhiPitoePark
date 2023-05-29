3bc3b701079a408682f09fc2ece61865657cce2e
andreas <s160419047@student.ubaya.ac.id>
2023-05-29 12:56:07 +0700

    update design nota pembayaran ke pelanggan

--- resources/views/transaction/invoicedesign.blade.php
+++ resources/views/transaction/invoicedesign.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+{{-- <!doctype html>
 <html lang="en">
 
 <head>
@@ -71,9 +71,62 @@
 
             </div>
         </div>
-    </div>
+    </div>  
     <!-- MDB -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.1/mdb.min.js"></script>
 </body>
 
-</html>
+</html> --}}
+@extends('layouts.auth')
+@section('content')
+    <div class="card card-outline">
+        <div class="card-header text-center">
+            <img src="{{ asset('template/assets/images/pitoe.png') }}" alt="logo" height="150" width="150">
+        </div>
+        <div class="card-body">
+            <b>No Meja : </b> <span class="font-weight-bold">12</span><br>
+            <b>Nama Pelanggan : </b> Andre<br>
+            <b>Nama Kasir : </b> Agus<br>
+            <b>Tanggal Transaksi : </b> 30 Mei 2023 12:30:00<br>
+            <b>Metode Pembayaran : </b> <span class="font-weight-bold text-uppercase font-italic">Tunai</span><br><hr>
+            <h3>Menu yang Dipesan</h3>
+            <b>Nasi Goreng</b><br>
+            <div class="row">
+               <div class="col-8"><b>1x</b>&nbsp;&nbsp;&nbsp;15.000</div>
+               <div class="col-4"><b>Rp. 15.000</b></div>
+            </div>
+            <b>Kentang Goreng</b><br>
+            <div class="row">
+               <div class="col-8"><b>2x</b>&nbsp;&nbsp;&nbsp;10.000</div>
+               <div class="col-4"><b>Rp. 20.000</b></div>
+            </div>
+            <b>Es Teh</b><br>
+            <div class="row">
+               <div class="col-8"><b>1x</b>&nbsp;&nbsp;&nbsp;5.000</div>
+               <div class="col-4"><b>Rp. 5.000</b></div>
+            </div><hr>
+            <h3>Rincian Pembayaran</h3>
+            <div class="row">
+               <div class="col-8"><b>Total Harga</b></div>
+               <div class="col-4"><b>Rp. 40.000</b></div>
+            </div>
+            <div class="row">
+               <div class="col-8"><b>Kendhi Pitoe E-Wallet</b></div>
+               <div class="col-4"><b>Rp. 40.000</b></div>
+            </div><br>
+            <div class="row">
+               <div class="col-8"><b>Harga Tunai</b></div>
+               <div class="col-4"><b>Rp. 50.000</b></div>
+            </div>
+            <div class="row">
+               <div class="col-8"><b>Harga Kembali</b></div>
+               <div class="col-4"><b>Rp. 10.000</b></div>
+            </div><hr>
+            <span class="text-center">
+               <h4>Matur Nuwun sampun</h4>
+               <h5>Rawuh ing</h5>
+               <h3>Kendhi Pitoe Park</h3>
+            </span>
+        </div>
+    </div>
+@endsection
