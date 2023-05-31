7acce891b3e6931a49d866063f47aef59af45177
andreas <s160419047@student.ubaya.ac.id>
2023-05-31 21:00:55 +0700

    tinggal buat view dan routing generate meja

--- app/Http/Controllers/MejaController.php
+++ app/Http/Controllers/MejaController.php
@@ -8,17 +8,17 @@ use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class MejaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $mejas = Meja::all();
-        dd($mejas);
-        return view('menu.index', compact("mejas"));
-    }
+   /**
+    * Display a listing of the resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+   public function index()
+   {
+      $mejas = Meja::all();
+      dd($mejas);
+      return view('menu.index', compact("mejas"));
+   }
 
    public function generate($id)
    {
@@ -26,10 +26,10 @@ class MejaController extends Controller
       $meja->link = md5($id);
       $meja->save();
       $qrcode = QrCode::size(400)
-               ->format('png')
-               ->merge('template/assets/images/pitoe.png')
-               ->errorCorrection('M') 
-               ->generate(route('order.generate', ['hash' => $meja->link]));
+         ->format('png')
+         ->merge('template/assets/images/pitoe.png')
+         ->errorCorrection('M')
+         ->generate(route('order.generate', ['hash' => $meja->link]));
       // return response()->streamDownload(
       //    function () {
       //       echo QrCode::size(200)
@@ -41,72 +41,81 @@ class MejaController extends Controller
       //       'Content-Type' => 'image/png',
       //    ]
       // );
-      return view('qrcode.detail', compact('qrcode'));
+      //return view('owner.generatemeja', compact('qrcode'));
    }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+   public function generateSignedUrl(string $hash)
+   {
+      $mejaModel = Meja::where('link', $hash)->firstOrFail();
+      $hashCode = md5(random_bytes(8));
+      session(['hash' => $hashCode, 'meja' => $mejaModel]);
+      //eturn redirect(route('order.index', ['hash' => $hashCode]));
+   }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+   
+   /**
+    * Show the form for creating a new resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+   public function create()
+   {
+      //
+   }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Meja  $meja
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Meja $meja)
-    {
-        //
-    }
+   /**
+    * Store a newly created resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @return \Illuminate\Http\Response
+    */
+   public function store(Request $request)
+   {
+      //
+   }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Meja  $meja
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Meja $meja)
-    {
-        //
-    }
+   /**
+    * Display the specified resource.
+    *
+    * @param  \App\Models\Meja  $meja
+    * @return \Illuminate\Http\Response
+    */
+   public function show(Meja $meja)
+   {
+      //
+   }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Meja  $meja
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Meja $meja)
-    {
-        //
-    }
+   /**
+    * Show the form for editing the specified resource.
+    *
+    * @param  \App\Models\Meja  $meja
+    * @return \Illuminate\Http\Response
+    */
+   public function edit(Meja $meja)
+   {
+      //
+   }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Meja  $meja
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Meja $meja)
-    {
-        //
-    }
+   /**
+    * Update the specified resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \App\Models\Meja  $meja
+    * @return \Illuminate\Http\Response
+    */
+   public function update(Request $request, Meja $meja)
+   {
+      //
+   }
+
+   /**
+    * Remove the specified resource from storage.
+    *
+    * @param  \App\Models\Meja  $meja
+    * @return \Illuminate\Http\Response
+    */
+   public function destroy(Meja $meja)
+   {
+      //
+   }
 }
