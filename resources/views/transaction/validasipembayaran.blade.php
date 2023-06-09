d2b661ba91a866c7f9b0a9dd041a9b91ff4f64cf
andreas <s160419047@student.ubaya.ac.id>
2023-06-09 08:22:36 +0700

    update view part 2

--- resources/views/transaction/validasipembayaran.blade.php
+++ resources/views/transaction/validasipembayaran.blade.php
@@ -1,4 +1,20 @@
 @extends('layouts.admin')
+@section('title')
+    Validasi Pembayaran
+@endsection
+@section('content-header')
+    <div class="row mb-2">
+        <div class="col-sm-6">
+            <h1 class="m-0"> Kendi Pitoe Cafe | <small>Validasi Pembayaran</small></h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+                <li class="breadcrumb-item"><a href="#">Validasi Pembayaran</a></li>
+            </ol>
+        </div><!-- /.col -->
+    </div><!-- /.row -->
+@endsection
 @section('content')
     <div class="container">
         <div class="card">
