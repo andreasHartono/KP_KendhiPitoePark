11dc9694b5b7aa3526804e9ceadccb384b8339d8
andreas <s160419047@student.ubaya.ac.id>
2023-06-13 15:59:01 +0700

    update error not found

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
@@ -34,4 +35,33 @@ class Handler extends ExceptionHandler
     {
         //
     }
+   /**
+    * Report or log an exception.
+    *
+    * @param  \Throwable  $exception
+    * @return void
+    *
+    * @throws \Exception
+    */
+   public function report(Throwable $exception)
+   {
+      parent::report($exception);
+   }
+
+   /**
+    * Render an exception into an HTTP response.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \Throwable  $exception
+    * @return \Symfony\Component\HttpFoundation\Response
+    *
+    * @throws \Throwable
+    */
+   public function render($request, Throwable $exception)
+   {
+      if ($this->isHttpException($exception)) {
+            return response()->view('errors.404', [], 404); 
+      }
+      return parent::render($request, $exception);
+   }
 }
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,24 +1,20 @@
 @extends('layouts.auth')
 @section('content')
     <!-- Main content -->
-    <section class="content">
-        <div class="error-page">
-            <h2 class="headline text-warning"> 404</h2>
-
+        <div class="error-page d-flex justify-content-center align-items-center">
             <div class="error-content">
+                <img src="{{ asset('images/404.svg') }}" alt="Error Image" class="img-responsive" height="300" width="300">
                 <h3><i class="fas fa-exclamation-triangle text-warning"></i> Oops, We can't seem to find the page you are
                     looking for.</h3>
-
                 <p>
                     The page you are looking for might have been removed, had its name changed, or is temporarily
                     unavailable.
-                    <a class="btn btn-success" href="{{ url('/') }}">return to main apps</a>
                 </p>
+                <a class="btn btn-success" href="{{ url('/') }}"><i class="fa fa-arrow-left"></i> Back to main apps</a>
                 </form>
             </div>
             <!-- /.error-content -->
         </div>
         <!-- /.error-page -->
-    </section>
     <!-- /.content -->
 @endsection
